1f97761fe734a20768370d608e3ec8ceae641c75
c2khanna <chaitanyakhanna13@gmail.com>
2021-01-04 13:54:01 -0500

    fix: 🐛 https requests

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://ec2-3-131-36-141.us-east-2.compute.amazonaws.com:3000'
+  apiUrl: 'https://ec2-3-131-36-141.us-east-2.compute.amazonaws.com:3000'
 };
